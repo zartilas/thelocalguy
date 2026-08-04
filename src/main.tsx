d13bfd49c4f7ead7081b738c79f9b248e8246024
Vasileios Zartilas <zartilasvasilios@protonmail.com>
2026-08-04 12:54:26 +0300

    Updated main, app and setup routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import {BrowserRouter} from 'react-router-dom'
+import {ChakraProvider, defaultSystem} from "@chakra-ui/react"
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider value={defaultSystem}>
             <BrowserRouter>
-                <App />
+                <App/>
             </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
