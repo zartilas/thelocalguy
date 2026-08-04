0a2c4be5e28c21fb9f5acf2b2e7d7934bd0aa620
Vasileios Zartilas <zartilasvasilios@protonmail.com>
2026-08-04 15:22:02 +0300

    Setup 404.html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import {Routes, Route} from 'react-router-dom';
+import {useEffect} from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import {Box} from '@chakra-ui/react';
 
 import Home from "./page/Home.tsx";
 import {NotFound} from "./page/NotFound.tsx";
 import {CookieBanner} from "./components/ui/cookie-banner.tsx";
 
-
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const redirectPath = sessionStorage.getItem('redirectPath');
+        if (redirectPath && redirectPath !== '/') {
+            sessionStorage.removeItem('redirectPath');
+            navigate(redirectPath, {replace: true});
+        }
+    }, [navigate]);
+
     return (
         <Box minH="100vh" display="flex" flexDirection="column">
-                <CookieBanner/>
+            <CookieBanner/>
+            <Box flex="1">
                 <Routes>
-                <Route
-                    path="/*"
-                    element={
-                        <>
-                            <Box flex="1">
-                                <Routes>
-                                    <Route path="/" element={<Home/>}/>
-                                    <Route path="*" element={<NotFound/>}/>
-                                </Routes>
-                            </Box>
-                        </>
-                    }
-                />
-            </Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Box>
         </Box>
     );
 }
