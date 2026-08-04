bae425931e3d64ba07787642ed6eee75e13fe8dd
Vasileios Zartilas <zartilasvasilios@protonmail.com>
2026-08-04 12:42:06 +0300

    Updated main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 // main.tsx
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { Provider } from './components/ui/provider'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import { NotFound } from './NotFound'
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider>
             <Router>
